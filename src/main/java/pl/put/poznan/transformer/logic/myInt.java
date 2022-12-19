9a3113166699ea5da3aba27dec29ba10fc4e233d
Andrew <olesik.andrei@gmail.com>
2022-12-19 16:22:06 +0100

    Visitor pattern

---
+++ src/main/java/pl/put/poznan/transformer/logic/myInt.java
@@ -0,0 +1,24 @@
+package pl.put.poznan.transformer.logic;
+
+public class myInt {
+    private int value;
+    public myInt(int a){
+        this.value=a;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void increment(){
+        setValue(getValue()+1);
+    }
+    public void reset(){
+        setValue(3);
+    }
+
+}
