5804a680f3f39c8772018a7261df2adf6f4fb3b2
wwiarenicz <wwiarenicz@gmail.com>
2022-12-19 18:38:53 +0100

    vv

---
+++ src/main/java/pl/put/poznan/transformer/rest/HelloContorller.java
@@ -0,0 +1,20 @@
+package pl.put.poznan.transformer.rest;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.base.Scenario;
+import pl.put.poznan.transformer.base.Step;
+import pl.put.poznan.transformer.base.SubScenario;
+import pl.put.poznan.transformer.logic.Visitor;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+@RestController
+public class HelloController {
+    @GetMapping("/first/{id}")
+    public String Reciving(@PathVariable String id) {
+        // log the parameters
+
+        return id;
+    }
+
+}
